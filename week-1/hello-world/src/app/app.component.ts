c168b1fc87b1b83e518edf8ec16d727364057fc5
fixedbits <fixingbitbybit@gmail.com>
2024-03-24 22:03:09 -0600

    week-1

---
+++ week-1/hello-world/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  myWorld = 'You are now in Soto\'s world!!';
+}
