196a3d56ff2eccc2ea860a355b950c3927719af4
fixedbits <fixingbitbybit@gmail.com>
2024-03-26 23:01:59 -0600

    profile-app

---
+++ week-1/profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,22 @@
+/**
+ * Title: my-details.component.ts
+ * Author: Victor Soto
+ * Date: 03/26/2024
+ * Description: My details component (Details view)
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
