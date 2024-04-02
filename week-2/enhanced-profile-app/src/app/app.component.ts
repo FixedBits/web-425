eefbf85516c3737055273aa37c655d5c5e7cbe35
fixedbits <fixingbitbybit@gmail.com>
2024-04-01 22:58:47 -0600

    enhanced-profile-app

---
+++ week-2/enhanced-profile-app/src/app/app.component.ts
@@ -0,0 +1,18 @@
+/**
+ * Title: app.component.ts
+ * Author: Victor Soto
+ * Date: 03/26/2024
+ * Description: App component
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  isLoggedIn: boolean = true;
+  assignment: string = 'Assignment 1.5 - Components';
+}
